edce13ded2a59ce53aef976fc59e6b41f04bde78
Tatan2002 <tatandgd@gmail.com>
2025-02-03 09:52:36 -0400

    http and mongo configured

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import { printHello } from '#Lib/hello.js';
+import connectDB from "#config/db.js";
+import '#config/env.js';
+import httpServer from "#config/https.js";
 
-printHello();
+
+const bootstrap = async () => {
+    
+    await connectDB(process.env.MONGODB_URL);
+
+    httpServer.listen(process.env.PORT, () => {console.log(`Servidor escuchando en el puerto ${process.env.port}`)});
+
+
+};
+
+bootstrap()
